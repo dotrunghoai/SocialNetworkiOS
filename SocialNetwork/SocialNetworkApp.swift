401f623354f39eed75a30c389ec589f429875936
Hoai <dotrunghoai@gmail.com>
2022-05-17 22:49:07 +0700

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Đỗ Trung Hoài on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
